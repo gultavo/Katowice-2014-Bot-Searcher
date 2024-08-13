5feb030f62a1f5b044d6b688d022aaa08d5c4a5e
gultavo <gusborsatto@gmail.com>
2024-08-12 23:09:39 -0300

    Delete Katowice 2014 BOT Searcher v1.0 directory

--- Katowice 2014 BOT Searcher v1.0/utils/chrome_find.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-chrome = webdriver.Chrome(options=options)
-find = chrome.find_element
--- Katowice 2014 BOT Searcher v1.0/utils/loader.py
+++
@@ -1,3 +0,0 @@
-from utils.func_loader import skins_module
-from run import chrome, find
-skins_module(chrome, find)
