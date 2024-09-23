1d2df2ac1f1b8156d2f56345c920720e35fcf725
gultavo <gusborsatto@gmail.com>
2024-09-22 22:59:32 -0300

    v1.0
    Used to stop autofinder.

---
+++ Katowice 2014 BOT Searcher v1.0/cogs/stop.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+from utils.functions import jsonregister
+import json
+class Stop(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        super().__init__()
+
+    @commands.command()
+    async def stop(self, ctx):
+        regist = json.load(open('autofindRegist.json'))
+        register = regist['register']
+        if register == 'True':
+            await ctx.send('Stopping autofinder...')
+            jsonregister('False', 'True')
+        else:
+            await ctx.send('Autofinder isnt activated, to activate it send .autofind')
+async def setup(bot):
+    await bot.add_cog(Stop(bot))
