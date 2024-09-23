b94c9801261c25a258039f97d142c821fc99f0cc
gultavo <gusborsatto@gmail.com>
2024-09-22 22:57:27 -0300

    v1.1
    Deleted time message.

--- Katowice 2014 BOT Searcher v1.0/cogs/list.py
+++ Katowice 2014 BOT Searcher v1.0/cogs/list.py
@@ -9,10 +9,8 @@ class List(commands.Cog):
     async def list(self, ctx:commands.Context):
         json_cog = json.load(open('varContent.json'))
         cogjson = json_cog['content']
-        timejson = json_cog['time']
         if len(cogjson) > 30:
             await ctx.send(cogjson)
-            await ctx.send(f'Last verified on: {timejson}')
         else: 
             await ctx.send('No skin was found')
 async def setup(bot):
