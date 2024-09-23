11f3bb1d18c48638dcf282bc1fd44574cef50db0
gultavo <gusborsatto@gmail.com>
2024-09-22 22:50:34 -0300

    v 1.1
    Less functionality imports

--- Katowice 2014 BOT Searcher v1.0/run.py
+++ Katowice 2014 BOT Searcher v1.0/run.py
@@ -1,10 +1,8 @@
-from time import sleep
 from utils.functions import cartridge, site
 from utils.chrome_find import *
-from utils.func_loader import *
 
 chrome.get(cartridge)
-sleep(4.0)
+# sleep(4.0)
 site(find, chrome)
 
-from utils.bot import *
+from utils.loader import *
