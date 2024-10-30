ee49bec0cfb3e75115d5f543cd7279c3b45d1ff7
gultavo <gusborsatto@gmail.com>
2024-10-30 15:18:14 -0300

    v1.15
    Fixed json's directories

--- Katowice 2014 BOT Searcher v1.0/cogs/list.py
+++ Katowice 2014 BOT Searcher v1.0/cogs/list.py
@@ -7,7 +7,7 @@ class List(commands.Cog):
     
     @commands.command()
     async def list(self, ctx:commands.Context):
-        json_cog = json.load(open('varContent.json'))
+        json_cog = json.load(open('./json/varContent.json'))
         cogjson = json_cog['content']
         if len(cogjson) > 30:
             await ctx.send(cogjson)
